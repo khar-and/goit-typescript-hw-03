1b0ffc3366f58f2af4b3eeef62be5de97e45ebef
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-11-02 12:01:51 +0200

    2done

--- src/2.ts
+++ src/2.ts
@@ -15,12 +15,11 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -32,8 +31,11 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+    // this.salary += 10000;
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
